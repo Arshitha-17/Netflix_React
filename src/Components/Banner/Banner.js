f9b8fd8ed608fb4f4ac3c4761200596c28cc8abc
Arshitha-17 <arshithaachu165@gmail.com>
2023-07-30 12:49:50 +0530

    youtube video

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -4,6 +4,7 @@ import './Banner.css'
 import axios  from '../../axios'
 function Banner() {
   const [movie, setmovie] = useState()
+
   useEffect(() => {
    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((responce)=>{
     console.log(responce.data.results[0]);
